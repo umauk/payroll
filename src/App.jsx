e839dc5bec356ea326d3647bead26f95deb88274
umauk <udaykirandevarasetti@gmail.com>
2024-11-22 16:40:23 +0530

    M

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
 import './App.css';
-import EmployeeList from './pages/Employeelist';
+import EmployeeLists from './pages/NempList';
+
 
 
 
@@ -27,7 +28,7 @@ function App() {
               <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/employees" exact element={<EmployeeList />} />
+                <Route path="/employees" exact element={<EmployeeLists/>} />
                 <Route path="/employees/:id" element={<EmployeeDetail />} />
                 <Route path="/payroll" element={<Payroll />} />
                 <Route path="/settings" element={<Settings />} />
